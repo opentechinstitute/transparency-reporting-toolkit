0680052eb271bc6570bea07cfd09d94a515bcd99
gembarrett <gem@gembarrett.com>
2016-03-23 17:06:37 -0400

    dealing with overlay between home and submit

---
+++ src/scripts/main.js
@@ -0,0 +1,5 @@
+var submitProcess = document.getElementsByClassName('overlay-background')[0];
+
+// document.getElementById("submit-report").addEventListener("click", function( event ) {
+//   submitProcess.style.display = "block";
+// }, false);
