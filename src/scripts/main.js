021af8b615ca5214e6c8f5dcb4fc930341b2148b
gembarrett <gem@gembarrett.com>
2016-04-01 12:51:57 -0700

    dealt with overlay

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,32 @@
 var submitProcess = document.getElementsByClassName('overlay-background')[0];
 
-// document.getElementById("submit-report").addEventListener("click", function( event ) {
-//   submitProcess.style.display = "block";
-// }, false);
+document.getElementById("submit-report").addEventListener("click", function( event ) {
+  event.preventDefault();
+  submitProcess.style.display = "block";
+}, false);
+
+calculator('request-types', 6);
+
+function calculator(section, maxNo) {
+
+  // get section
+  el = document.getElementsByClassName(section);
+  console.log(el[0]);
+  // get all inputs in that section
+  allInputs = el[0].getElementsByTagName('input');
+  console.log(allInputs);
+  // get total box
+  totalInput = allInputs[maxNo];
+  total = 0;
+  allInputs[maxNo].value = 0;
+  // filter down to only required ones
+  if (this.value != "") {
+    for (var i=0; i<maxNo;i++){
+      allInputs[i].oninput = function() {
+        total = parseFloat(this.value+total);
+        console.log(total);
+        totalInput.value = total;
+      }
+    }
+  }
+}
