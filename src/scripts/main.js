d0944821cce045db1287e71f2a4aacb445fe624e
gembarrett <gem@gembarrett.com>
2016-05-13 16:01:00 -0400

    triggering validation from js when nav link clicked

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,47 @@
 // overlay elements
 var submitProcess = [document.getElementsByClassName('overlay-background')[0], document.getElementsByClassName('overlay')[0]];
+// submission link
 var submitLink = document.getElementById("submit-link");
-
-var user;
+// check if we're on home page
+function isHome() {
+  if(document.getElementById('originalCompany')) {
+    return true;
+  } else {
+    return false;
+  }
+}
+// second company name input box
 var copyCompany = document.getElementById('copyCompany');
+// if home
+if (isHome()) {
+  // hide second box
+  copyCompany.style.visibility = "hidden";
+  // grab first box
+  var originalCompany = document.getElementById('originalCompany');
+  // grab submit report button
+  var submitButton = document.getElementById("submit-report");
+  // add click trigger to submit report button and nav link
+  clickTrigger(submitButton);
+  clickTrigger(submitLink);
+}
+
+var element = document.getElementById("to-form");
+
+function clickTrigger(el) {
+  el.addEventListener("click", function(event) {
+    event.preventDefault();
+    console.log(originalCompany.validationMessage);
+    // is there an entry in the text box?
+    if (originalCompany.checkValidity()) {
+      console.log('yep');
+      // show overlay and copy inputted value to hidden text box
+      showOverlay();
+      copyCompany.value = originalCompany.value;
+    } else {
+      originalCompany.focus();
+    }
+  }, false);
+}
 
 /* nav link:
 - if home page then validate main box value, show overlay, copy to 2nd box and hide it
@@ -13,43 +51,7 @@ button link (only home page):
 - validate main box value, show overlay, copy to 2nd box and hide it
 */
 
-// function to check for element existence
-function itLives(el) {
-  if(document.getElementById(el)) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-// get inputted name from textbox, if it exists, else empty for now
-if (itLives('originalCompany')) {
-  var originalCompany = document.getElementById('originalCompany');
-} else {
-  var originalCompany = '';
-}
-
-// TODO: hook this up to Directory API to check against group member list
-var approved = ['gembarrett1986@gmail.com'];
-function include(arr,obj) {
-    return (arr.indexOf(obj) != -1);
-}
-
-// if we're on the home page, check for company name before proceeding
-if (itLives("submit-report")) {
-  var homeButton = document.getElementById("submit-report");
-  homeButton.addEventListener("click", function(event) {
-    // is there an entry in the text box?
-    if (originalCompany.checkValidity()) {
-      // show overlay and copy inputted value to hidden text box
-      showOverlay();
-      copyCompany.value = originalCompany.value;
-    }
-  }, false);
-}
-
 function showOverlay() {
-  event.preventDefault();
   submitProcess[0].style.display = "block";
   submitProcess[1].style.display = "block";
   document.getElementsByTagName('body')[0].style.overflow = "hidden";
@@ -61,6 +63,18 @@ submitProcess[0].addEventListener("click", function( event ) {
   document.getElementsByTagName('body')[0].style.overflow = "initial";
   copyCompany.value = "";
 }, false);
+
+// sign-in and authentication
+var user;
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  user = profile.getEmail();
+}
+// TODO: hook this up to Directory API to check against group member list
+var approved = ['gembarrett1986@gmail.com'];
+function include(arr,obj) {
+    return (arr.indexOf(obj) != -1);
+}
 
 document.getElementById("to-form").addEventListener("click", function( event ) {
   var isApproved = include(approved, user);
@@ -74,8 +88,3 @@ document.getElementById("to-form").addEventListener("click", function( event ) {
     event.preventDefault();
   }
 }, false);
-
-function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
-  user = profile.getEmail();
-}
