8ec5318c7112e8164d5a0c136008a912cd5f5338
gembarrett <gem@gembarrett.com>
2016-05-23 09:01:45 -0400

    passing second page data through

--- dest/scripts/submit-1.php
+++ dest/scripts/submit-1.php
@@ -6,4 +6,7 @@ $_SESSION['usa_other-orders_total'] = $_POST['other-court-orders'];
 $_SESSION['usa_criminal-subpoenas_total'] = $_POST['criminal-subpoenas'];
 $_SESSION['usa_civil-subpoenas_total'] = $_POST['civil-subpoenas'];
 $_SESSION['usa_emergency-requests_total'] = $_POST['emergency-requests'];
+$_SESSION['usa_requests-total'] = $_POST['usa-types-total'];
+$_SESSION['usa_types-selectors-specified'] = $_POST['usa-types-selectors-specified'];
+$_SESSION['usa_accounts-responsive'] = $_POST['usa-types-accounts-responsive'];
 ?>
--- dest/scripts/submit-2.php
+++ dest/scripts/submit-2.php
@@ -1,2 +1,11 @@
 <?php
+$_SESSION['usa_search-warrants_rejected_number'] = $_POST['search-rejected'];
+$_SESSION['usa_search-warrants_rejected_perc'] = $_POST['search-pc-rejected'];
+$_SESSION['usa_search-warrants_no-data_number'] = $_POST['search-no-data'];
+$_SESSION['usa_search-warrants_no-data_perc'] = $_POST['search-pc-no-data'];
+$_SESSION['usa_search-warrants_content-disc_number'] = $_POST['search-content-disclosed'];
+$_SESSION['usa_search-warrants_content-disc_perc'] = $_POST['search-pc-content-disclosed'];
+$_SESSION['usa_search-warrants_non-content-disc_number'] = $_POST['search-non-content-disclosed'];
+$_SESSION['usa_search-warrants_non-content-disc_perc'] = $_POST['search-pc-non-content-disclosed'];
+$_SESSION['usa_search-warrants_total_validation'] = $_POST['search-total'];
 ?>
