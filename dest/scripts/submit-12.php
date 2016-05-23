c578e19d8578b6a9058301fd0fba01021c3bf9af
gembarrett <gem@gembarrett.com>
2016-05-23 09:51:16 -0400

    post data all collected

---
+++ dest/scripts/submit-12.php
@@ -0,0 +1,11 @@
+<?php
+$_SESSION['non-usa_compliance_rejected_number'] = $_POST['non-usa-outcomes-rejected'];
+$_SESSION['non-usa_compliance_rejected_perc'] = $_POST['non-usa-outcomes-pc-rejected'];
+$_SESSION['non-usa_compliance_no-data_number'] = $_POST['non-usa-outcomes-no-data'];
+$_SESSION['non-usa_compliance_no-data_perc'] = $_POST['non-usa-outcomes-pc-no-data'];
+$_SESSION['non-usa_compliance_content-disc_number'] = $_POST['non-usa-outcomes-content-disclosed'];
+$_SESSION['non-usa_compliance_content-disc_perc'] = $_POST['non-usa-outcomes-pc-content-disclosed'];
+$_SESSION['non-usa_compliance_non-content-disc_number'] = $_POST['non-usa-outcomes-non-content-disclosed'];
+$_SESSION['non-usa_compliance_non-content-disc_perc'] = $_POST['non-usa-outcomes-pc-non-content-disclosed'];
+$_SESSION['non-usa_requests_total_validation'] = $_POST['non-usa-outcomes-total'];
+?>
--- dest/scripts/submit-13.php
+++ dest/scripts/submit-13.php
@@ -1,5 +1,14 @@
 <?php
 
+$_SESSION['non-usa_user-notification_sealed_number'] = $_POST['non-usa-notifications-sealed'];
+$_SESSION['non-usa_user-notification_sealed_perc'] = $_POST['non-usa-notifications-pc-sealed'];
+$_SESSION['non-usa_user-notification_not-sealed-notice_number'] = $_POST['non-usa-notifications-not-sealed-notice'];
+$_SESSION['non-usa_user-notification_not-sealed-notice_perc'] = $_POST['non-usa-notifications-pc-not-sealed-notice'];
+$_SESSION['non-usa_user-notification_not-sealed_number'] = $_POST['non-usa-notifications-not-sealed-no-notice'];
+$_SESSION['non-usa_user-notification_not-sealed_perc'] = $_POST['non-usa-notifications-pc-not-sealed-no-notice'];
+$_SESSION['non-usa_user-notification_total'] = $_POST['non-usa-notifications-total'];
+
+
 function getKey() {
   if ($handle = opendir('./scripts/resources/')) {
       while (false !== ($entry = readdir($handle))) {
