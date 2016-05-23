3c36b16eb96c1e5ff325214722a20a9cae96ada5
gembarrett <gem@gembarrett.com>
2016-05-23 09:13:28 -0400

    more post data set up

---
+++ dest/scripts/submit-4.php
@@ -0,0 +1,11 @@
+<?php
+$_SESSION['usa_pen-registers_rejected_number'] = $_POST['pens-rejected'];
+$_SESSION['usa_pen-registers_rejected_perc'] = $_POST['pens-pc-rejected'];
+$_SESSION['usa_pen-registers_no-data_number'] = $_POST['pens-no-data'];
+$_SESSION['usa_pen-registers_no-data_perc'] = $_POST['pens-pc-no-data'];
+$_SESSION['usa_pen-registers_content-disc_number'] = $_POST['pens-content-disclosed'];
+$_SESSION['usa_pen-registers_content-disc_perc'] = $_POST['pens-pc-content-disclosed'];
+$_SESSION['usa_pen-registers_non-content-disc_number'] = $_POST['pens-non-content-disclosed'];
+$_SESSION['usa_pen-registers_non-content-disc_perc'] = $_POST['pens-pc-non-content-disclosed'];
+$_SESSION['usa_pen-registers_total_validation'] = $_POST['pens-total'];
+?>
---
+++ dest/scripts/submit-5.php
@@ -0,0 +1,11 @@
+<?php
+$_SESSION['usa_other-orders_rejected_number'] = $_POST['other-rejected'];
+$_SESSION['usa_other-orders_rejected_perc'] = $_POST['other-pc-rejected'];
+$_SESSION['usa_other-orders_no-data_number'] = $_POST['other-no-data'];
+$_SESSION['usa_other-orders_no-data_perc'] = $_POST['other-pc-no-data'];
+$_SESSION['usa_other-orders_content-disc_number'] = $_POST['other-content-disclosed'];
+$_SESSION['usa_other-orders_content-disc_perc'] = $_POST['other-pc-content-disclosed'];
+$_SESSION['usa_other-orders_non-content-disc_number'] = $_POST['other-non-content-disclosed'];
+$_SESSION['usa_other-orders_non-content-disc_perc'] = $_POST['other-pc-non-content-disclosed'];
+$_SESSION['usa_other-orders_total_validation'] = $_POST['other-total'];
+?>
