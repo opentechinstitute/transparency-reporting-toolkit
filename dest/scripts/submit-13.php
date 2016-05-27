70f780375e5379bf819d51462024a38dad30c4fb
gembarrett <gem@gembarrett.com>
2016-05-27 14:31:34 -0400

    committing because broken fields, will fix once I've worked out how

--- dest/scripts/submit-13.php
+++ dest/scripts/submit-13.php
@@ -1,4 +1,5 @@
 <?php
+date_default_timezone_set('America/New_York');
 
 $_SESSION['non_usa_user_notification_sealed_number'] = $_POST['non-usa-notifications-sealed'];
 $_SESSION['non_usa_user_notification_sealed_perc'] = $_POST['non-usa-notifications-pc-sealed'];
@@ -7,6 +8,8 @@ $_SESSION['non_usa_user_notification_not_sealed_notice_perc'] = $_POST['non-usa-
 $_SESSION['non_usa_user_notification_not_sealed_number'] = $_POST['non-usa-notifications-not-sealed-no-notice'];
 $_SESSION['non_usa_user_notification_not_sealed_perc'] = $_POST['non-usa-notifications-pc-not-sealed-no-notice'];
 $_SESSION['non_usa_user_notification_total'] = $_POST['non-usa-notifications-total'];
-
-print_r($_SESSION);
+$date = new DateTime();
+$date->getTimestamp();
+$_SESSION['submission_timestamp'] = $date->format('Y-m-d H:i:s');
+print_r($_SESSION['submission_timestamp']);
 ?>
